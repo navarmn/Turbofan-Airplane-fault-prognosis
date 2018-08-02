bfcc64e3f6415eea19e58cb2ae5f9f9904fa1a3f
Navar M M N <navarmedeiros@gmail.com>
2018-08-02 17:47:44 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A framework for analyses in the prognosis dataset PHM08 provided by NASA. This project is part of my Master's degree thesis.',
+    author='Navar M. M. Nascimento',
+    license='MIT',
+)
